c39ab343dbf3b2225ffd2710effd5c679d5715b7
ayengec <83873798+ayengec@users.noreply.github.com>
2021-07-12 12:02:14 +0300

    Update main.c

--- UART_w_Interrupt/main.c
+++ UART_w_Interrupt/main.c
@@ -1,3 +1,5 @@
+// ayengec LM4F120XL Stellaris Board Interrupt UART with RGB LEDs
+
 #include "inc/hw_ints.h"
 #include "inc/hw_memmap.h"
 #include "inc/hw_types.h"
@@ -10,152 +12,107 @@
 #include "driverlib/sysctl.h"
 #include "driverlib/uart.h"
 
-
-//*****************************************************************************
-//
-// The UART interrupt handler.
-//
-//*****************************************************************************
 void UARTSend(const unsigned char *pucBuffer, unsigned long ulCount)
 {
-    //
-    // Loop while there are more characters to send.
-    //
     while(ulCount--)
     {
-        //
-        // Write the next character to the UART.
-        //
+
         ROM_UARTCharPutNonBlocking(UART0_BASE, *pucBuffer++);
     }
 }
-void UARTIntHandler(void)
+void myUARTIntHandler(void)
 {
     unsigned long ulStatus;
 
-    //
     // Get the interrrupt status.
-    //
     ulStatus = ROM_UARTIntStatus(UART0_BASE, true);
 
-    //
     // Clear the asserted interrupts.
-    //
     ROM_UARTIntClear(UART0_BASE, ulStatus);
 
-    //
     // Loop while there are characters in the receive FIFO.
-    //
     while(ROM_UARTCharsAvail(UART0_BASE))
     {
         //
         // Read the next character from the UART and write it back to the UART.
         //
-        ROM_UARTCharPutNonBlocking(UART0_BASE,
-                                   ROM_UARTCharGetNonBlocking(UART0_BASE));
-			  UARTSend((unsigned char *)" CHAR IS SENT\r\n",25);
-
+        ROM_UARTCharPutNonBlocking(UART0_BASE, ROM_UARTCharGetNonBlocking(UART0_BASE));
+			  UARTSend((unsigned char *)"INTERRUPTED\r\n",14);
+        // Blink the LEDs as white
+        GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3, 15);
         
-        //
-        // Blink the LED to show a character transfer is occuring.
-        //
-        GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_2, GPIO_PIN_2);
+        SysCtlDelay(16000000);
         
+        // Turn off the LEDs
+			  ROM_UARTCharPutNonBlocking(UART0_BASE, ROM_UARTCharGetNonBlocking(UART0_BASE));
+			  UARTSend((unsigned char *)"CONTINUING\r\n",15);
         //
-        // Delay for 1 millisecond.  Each SysCtlDelay is about 3 clocks.
-        //
-        SysCtlDelay(SysCtlClockGet() / (1000 * 3));
-        
-        //
-        // Turn off the LED
-        //
-        GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_2, 0);
+        GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3, 0);
         
     }
 }
 
-//*****************************************************************************
-//
-// Send a string to the UART.
-//
-//*****************************************************************************
-
-
-//*****************************************************************************
-//
-// This example demonstrates how to send a string of data to the UART.
-//
-//*****************************************************************************
 int main(void)
 {
-    //
-    // Enable lazy stacking for interrupt handlers.  This allows floating-point
-    // instructions to be used within interrupt handlers, but at the expense of
-    // extra stack usage.
-    //
+    // Enable lazy stacking for interrupt handlers
     ROM_FPUEnable();
     ROM_FPULazyStackingEnable();
 
-    //
-    // Set the clocking to run directly from the crystal.
-    //
     ROM_SysCtlClockSet(SYSCTL_SYSDIV_1 | SYSCTL_USE_OSC | SYSCTL_OSC_MAIN |
                        SYSCTL_XTAL_16MHZ);
 
-    //
     // Enable the GPIO port that is used for the on-board LED.
-    //
     ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
 
-    //
-    // Enable the GPIO pins for the LED (PF2).  
-    //
-    ROM_GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_2);
+    // Enable the GPIO pins for the LEDs 
+    ROM_GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3);
 
-    //
     // Enable the peripherals used by this example.
-    //
     ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_UART0);
     ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
 
-    //
     // Enable processor interrupts.
-    //
     ROM_IntMasterEnable();
 
-    //
     // Set GPIO A0 and A1 as UART pins.
-    //
     GPIOPinConfigure(GPIO_PA0_U0RX);
     GPIOPinConfigure(GPIO_PA1_U0TX);
     ROM_GPIOPinTypeUART(GPIO_PORTA_BASE, GPIO_PIN_0 | GPIO_PIN_1);
 
-    //
-    // Configure the UART for 115,200, 8-N-1 operation.
-    //
+    // Configure the UART for 115,200, 8 bit data, 1 stop, none parity
     ROM_UARTConfigSetExpClk(UART0_BASE, ROM_SysCtlClockGet(), 115200,
                             (UART_CONFIG_WLEN_8 | UART_CONFIG_STOP_ONE |
                              UART_CONFIG_PAR_NONE));
 
-    //
     // Enable the UART interrupt.
-    //
     ROM_IntEnable(INT_UART0);
     ROM_UARTIntEnable(UART0_BASE, UART_INT_RX | UART_INT_RT);
 
-    //
-    // Prompt for text to be entered.
-    //
+		UARTSend((unsigned char *)"****ayengec****\r\n", 20);
+		SysCtlDelay(800000);
     UARTSend((unsigned char *)"Program Started\r\n", 20);
 
-    //
-    // Loop forever echoing data through the UART.
-    //
+		// Red, green and blue sequentially lights but if any uart message received, all LEDs will emit as WHITE color 
     while(1)
     {
 			SysCtlDelay(8000000);
-			UARTSend((unsigned char *)"alican\r\n",10);
+			
+			GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3, 2);
+			UARTSend((unsigned char *)"RED LED\r\n",11);
 			SysCtlDelay(8000000);
-			UARTSend((unsigned char *)"yengec\r\n",10);
+			GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3, 0);
+			
+			
+			GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3, 4);
+			UARTSend((unsigned char *)"BLUE LED\r\n",12);
+			SysCtlDelay(8000000);
+			GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3, 0);
+			
+			
+			GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3, 8);
+			UARTSend((unsigned char *)"GREEN LED\r\n",12);
+			SysCtlDelay(8000000);
+			GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3, 0);
+			
     }
 }
